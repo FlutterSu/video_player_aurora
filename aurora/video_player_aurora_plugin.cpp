6ae74b6a6cae9090fb1b43099b46cf8b7238e5a9
Alexander Syrykh <asyrykh@yandex.ru>
2023-12-14 10:50:45 +0300

    + added gst_video_player

--- aurora/video_player_aurora_plugin.cpp
+++ aurora/video_player_aurora_plugin.cpp
@@ -1,3 +1,7 @@
+// Copyright 2023 Alexander Syrykh. All rights reserved.
+// Copyright 2021 Sony Group Corporation. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
 #include <video_player_aurora/video_player_aurora_plugin.h>
 #include <sys/utsname.h>
 

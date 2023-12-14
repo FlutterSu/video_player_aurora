87e6a3a9be47571ffe129fc0a4bd3fe551bce085
Alexander Syrykh <asyrykh@yandex.ru>
2023-12-14 21:39:34 +0300

    + added example

--- aurora/gst_video_player.h
+++ aurora/gst_video_player.h
@@ -1,3 +1,4 @@
+// Copyright 2023 Alexander Syrykh. All rights reserved.
 // Copyright 2021 Sony Group Corporation. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
---
+++ example/README.md
@@ -0,0 +1,7 @@
+# video_player_example
+
+Demonstrates how to use the video_player plugin.
+
+# Getting Started
+
+For help getting started with Flutter for Aurora OS , view our online documentation.
